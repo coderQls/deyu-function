9aa4745cada1ddd59046bfaad1e632f8db339cc3
coderQls <1064341174@qq.com>
2022-12-01 16:18:57 +0800

    feat: 添加ts编译

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'deyuFunction'
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        exclude: '/node_modules/',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: {
+                      chrome: "58",
+                      ie: "11"
+                    },
+                    corejs: 3,
+                    useBuiltIns: "usage"
+                  }
+                ]
+              ]
+            }
+          },
+          {
+            loader: 'ts-loader',
+          }
+        ]
+      }
+    ]
+  }
+}
